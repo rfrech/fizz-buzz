a139b9b707ad1b70bb671c7c854205974f483f9b
rfrech <rfrechette@cnm.edu>
2020-09-23 11:08:52 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/view/FizzBuzzString.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.view;
+
+import edu.cnm.deepdive.model.FizzBuzz;
+
+public class FizzBuzzString {
+
+  public static String format(FizzBuzz value) {
+    return value.toString().toLowerCase().replace('_', '-');
+  }
+
+}
